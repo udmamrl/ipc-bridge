55a05db853ffb46b30c93fe9a354e11b7ebeee71
Cheng-Lung Lee <chenglung@gmail.com>
2013-05-22 13:00:43 -0400

    use rospack find ipc to find xdrgen location for message code generation

--- ipc_bridge/generate_msgs.sh
+++ ipc_bridge/generate_msgs.sh
@@ -5,5 +5,5 @@ mkdir -p $1/include/ipc_bridge/msgs
 for i in xdr/*xdr;
 do
     echo "Generating `basename $i xdr`h"
-    xdrgen --directives=none $i $1/include/ipc_bridge/msgs/`basename $i xdr`h
+    $(rospack find ipc)/bin/xdrgen --directives=none $i $1/include/ipc_bridge/msgs/`basename $i xdr`h
 done
