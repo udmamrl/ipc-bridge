ebf04416d7b88541484f04e468bf15e925839e7e
Cheng-Lung Lee <chenglung@gmail.com>
2013-05-22 14:23:10 -0400

    fix the include and lib for ipc

--- ipc_bridge_ros/ipc_bridge_ros.mk
+++ ipc_bridge_ros/ipc_bridge_ros.mk
@@ -1,9 +1,12 @@
 ROS_CXXFLAGS = $(PKG_CXXFLAGS)
 ROS_CXXFLAGS += $(foreach path, $(shell rospack cflags-only-I $(PKG)), -I$(path))
 ROS_CXXFLAGS += $(foreach path, $(shell rospack cflags-only-I ipc_bridge_ros), -I$(path))
+ROS_CXXFLAGS += $(foreach path, $(shell rospack cflags-only-I ipc), -I$(path))
 
 ROS_LFLAGS = $(shell rospack libs-only-other roscpp)
 ROS_LFLAGS += $(foreach path, $(shell rospack libs-only-L roscpp), -L$(path))
+ROS_LFLAGS += $(foreach path, $(shell rospack libs-only-L ipc)   , -L$(path))
+
 ROS_LIBS = $(foreach lib, $(shell rospack libs-only-l roscpp), -l$(lib))
 
 ros: $(ROS_FILES)
