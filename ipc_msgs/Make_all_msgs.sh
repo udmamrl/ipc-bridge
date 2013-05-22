da8a1d8811e7089adf8025a08c9a2306e6c5baea
Cheng-Lung Lee <chenglung@gmail.com>
2013-05-22 17:03:35 -0400

    add Make_all_msgs.sh for easy make, also remove unused include header files , move ipc.h to right place

---
+++ ipc_msgs/Make_all_msgs.sh
@@ -0,0 +1,13 @@
+#rosmake ipc_msgs
+#read -p "Make sure there is no error during rosmake then -> Press [Enter] key to continue"
+rosmake ipc_rosgraph_msgs
+read -p "Make sure there is no error during rosmake then -> Press [Enter] key to continue"
+rosmake ipc_std_msgs
+read -p "Make sure there is no error during rosmake then -> Press [Enter] key to continue"
+rosmake ipc_geometry_msgs
+read -p "Make sure there is no error during rosmake then -> Press [Enter] key to continue"
+rosmake ipc_nav_msgs
+read -p "Make sure there is no error during rosmake then -> Press [Enter] key to continue"
+rosmake ipc_sensor_msgs
+read -p "Make sure there is no error during rosmake then -> Press [Enter] key to continue"
+
