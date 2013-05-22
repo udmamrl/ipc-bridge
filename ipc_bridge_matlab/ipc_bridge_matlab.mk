65217274ff81678c2f86ef32acc99edf8e3bbf34
Cheng-Lung Lee <chenglung@gmail.com>
2013-05-22 13:46:51 -0400

    add makefile flag link for ipc_bridge_matlab

--- ipc_bridge_matlab/ipc_bridge_matlab.mk
+++ ipc_bridge_matlab/ipc_bridge_matlab.mk
@@ -1,6 +1,7 @@
 MATLAB_CXXFLAGS = $(PKG_CXXFLAGS)
 MATLAB_CXXFLAGS += $(foreach path, $(shell rospack cflags-only-I ipc_bridge_matlab), -I$(path))
-
+MATLAB_CXXFLAGS += $(foreach path, $(shell rospack cflags-only-I ipc), -I$(path))
+MATLAB_CXXFLAGS += $(foreach path, $(shell rospack libs-only-L ipc), -L$(path))
 mex: $(MEX_FILES)
 
 %: mex/%.cc
